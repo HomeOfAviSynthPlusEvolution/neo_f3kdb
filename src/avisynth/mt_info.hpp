74c28e0a388598d782b793bbfd42b0f8745d173e
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2018-02-26 00:35:32 +0100

    Kill more tabs

--- src/avisynth/mt_info.hpp
+++ src/avisynth/mt_info.hpp
@@ -10,59 +10,59 @@
 typedef struct _mt_info
 {
     int n;
-	PVideoFrame src;
-	PVideoFrame dst;
-	unsigned char *dstp_u;
-	unsigned char *dstp_v;
-	IScriptEnvironment* env;
+    PVideoFrame src;
+    PVideoFrame dst;
+    unsigned char *dstp_u;
+    unsigned char *dstp_v;
+    IScriptEnvironment* env;
 
-	bool exit;
+    bool exit;
 
-	HANDLE thread_handle;
-	HANDLE work_event;
-	HANDLE work_complete_event;
+    HANDLE thread_handle;
+    HANDLE work_event;
+    HANDLE work_complete_event;
 } mt_info;
 
 static mt_info* mt_info_create(void) {
-	mt_info* ret = (mt_info*)malloc(sizeof(mt_info));
-	if (ret) {
-		memset(ret, 0, sizeof(mt_info));
-		ret->work_complete_event = CreateEvent(NULL, FALSE, FALSE, NULL);
-		ret->work_event = CreateEvent(NULL, FALSE, FALSE, NULL);
-		if (!ret->work_complete_event || !ret->work_event) {
-			CloseHandle(ret->work_complete_event);
-			CloseHandle(ret->work_event);
-			free(ret);
-			ret = NULL;
-		}
-	}
-	return ret;
+    mt_info* ret = (mt_info*)malloc(sizeof(mt_info));
+    if (ret) {
+        memset(ret, 0, sizeof(mt_info));
+        ret->work_complete_event = CreateEvent(NULL, FALSE, FALSE, NULL);
+        ret->work_event = CreateEvent(NULL, FALSE, FALSE, NULL);
+        if (!ret->work_complete_event || !ret->work_event) {
+            CloseHandle(ret->work_complete_event);
+            CloseHandle(ret->work_event);
+            free(ret);
+            ret = NULL;
+        }
+    }
+    return ret;
 }
 
 static void mt_info_reset_pointers(volatile mt_info* info) {
-	assert(info);
+    assert(info);
 
-	auto info_nv = const_cast<mt_info*>(info);
-	info_nv->dstp_u = NULL;
-	info_nv->dstp_v = NULL;
-	info_nv->src = NULL;
-	info_nv->dst = NULL;
-	MemoryBarrier();
+    auto info_nv = const_cast<mt_info*>(info);
+    info_nv->dstp_u = NULL;
+    info_nv->dstp_v = NULL;
+    info_nv->src = NULL;
+    info_nv->dst = NULL;
+    MemoryBarrier();
 }
 
 static void mt_info_destroy(volatile mt_info* info) {
-	if (!info) {
-		return;
-	}
-	
-	assert(info->work_complete_event);
-	assert(info->work_event);
+    if (!info) {
+        return;
+    }
+    
+    assert(info->work_complete_event);
+    assert(info->work_event);
 
-	mt_info_reset_pointers(info);
-	
-	CloseHandle(info->work_complete_event);
-	CloseHandle(info->work_event);
-	CloseHandle(info->thread_handle);
+    mt_info_reset_pointers(info);
+    
+    CloseHandle(info->work_complete_event);
+    CloseHandle(info->work_event);
+    CloseHandle(info->thread_handle);
 
-	free((void*)info);
+    free((void*)info);
 }
