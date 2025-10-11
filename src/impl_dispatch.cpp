677a40142570cb6e71f35a4bd89ab50f0351b36d
Roman Volkov <riv19-dev@pm.me>
2025-10-11 09:53:18 +0200

    Fix undefined symbols on non-x86 platforms
        Warning: Failed to load /usr/lib/vapoursynth/libneo-f3kdb.so. Error
        given: /usr/lib/vapoursynth/libneo-f3kdb.so: undefined symbol:
        process_plane_impl_sse4_16bit_interleaved

--- src/impl_dispatch.cpp
+++ src/impl_dispatch.cpp
@@ -8,36 +8,60 @@ const process_plane_impl_t* process_plane_impl_high_precision_no_dithering[] = {
     process_plane_impl_c_high_no_dithering,
     process_plane_impl_c_high_no_dithering,
     process_plane_impl_c_high_no_dithering,
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386) || defined(_M_IX86)
     process_plane_impl_sse4_high_no_dithering,
     process_plane_impl_avx2_high_no_dithering,
-    process_plane_impl_avx512_high_no_dithering
+    process_plane_impl_avx512_high_no_dithering,
+#else
+    nullptr,
+    nullptr,
+    nullptr,
+#endif
 };
 
 const process_plane_impl_t* process_plane_impl_high_precision_ordered_dithering[] = {
     process_plane_impl_c_high_ordered_dithering,
     process_plane_impl_c_high_ordered_dithering,
     process_plane_impl_c_high_ordered_dithering,
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386) || defined(_M_IX86)
     process_plane_impl_sse4_high_ordered_dithering,
     process_plane_impl_avx2_high_ordered_dithering,
-    process_plane_impl_avx512_high_ordered_dithering
+    process_plane_impl_avx512_high_ordered_dithering,
+#else
+    nullptr,
+    nullptr,
+    nullptr,
+#endif
 };
 
 const process_plane_impl_t* process_plane_impl_high_precision_floyd_steinberg_dithering[] = {
     process_plane_impl_c_high_floyd_steinberg_dithering,
     process_plane_impl_c_high_floyd_steinberg_dithering,
     process_plane_impl_c_high_floyd_steinberg_dithering,
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386) || defined(_M_IX86)
     process_plane_impl_sse4_high_floyd_steinberg_dithering,
     process_plane_impl_avx2_high_floyd_steinberg_dithering,
-    process_plane_impl_avx512_high_floyd_steinberg_dithering
+    process_plane_impl_avx512_high_floyd_steinberg_dithering,
+#else
+    nullptr,
+    nullptr,
+    nullptr,
+#endif
 };
 
 const process_plane_impl_t* process_plane_impl_16bit_interleaved[] = {
     process_plane_impl_c_16bit_interleaved,
     process_plane_impl_c_16bit_interleaved,
     process_plane_impl_c_16bit_interleaved,
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386) || defined(_M_IX86)
     process_plane_impl_sse4_16bit_interleaved,
     process_plane_impl_avx2_16bit_interleaved,
-    process_plane_impl_avx512_16bit_interleaved
+    process_plane_impl_avx512_16bit_interleaved,
+#else
+    nullptr,
+    nullptr,
+    nullptr,
+#endif
 };
 
 
